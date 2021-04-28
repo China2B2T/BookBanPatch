0a54e523a39024fe80b143a838adee2106c551ff
401395069 <401395069@qq.com>
2021-04-28 11:54:23 +0800

    Use listener instead

--- src/main/java/org/china2b2t/nobookban/Main.java
+++ src/main/java/org/china2b2t/nobookban/Main.java
@@ -2,23 +2,32 @@ package org.china2b2t.nobookban;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-            public void run() {
-                for(Player player : getServer().getOnlinePlayers()) {
-                    if (player.getInventory().getItemInMainHand().getType() == Material.WRITTEN_BOOK && ((BookMeta) player.getInventory().getItemInMainHand().getItemMeta()).getPage(1).contains("\u0800") && ((BookMeta) player.getInventory().getItemInMainHand().getItemMeta()).getPageCount() >= 1) {
-                        player.sendMessage(ChatColor.RED + "Sorry, a symbol you used in the book is currently blocked due to an exploit.");
-                        player.getInventory().setItemInMainHand(new ItemStack(Material.WRITABLE_BOOK));
-                    }
-                }
-            }
-        }, 1l, 1l);
+		getServer().getPluginManager().registerEvents(this, this);
     }
+	
+	@EventHandler
+	public void onInteract(InventoryInteractEvent e) {
+		HumanEntity player = e.getWhoClicked();
+		
+		if(player.isOp()) {
+			return;
+		}
+		if (player.getInventory().getItemInMainHand().getType() == Material.WRITTEN_BOOK && ((BookMeta) player.getInventory().getItemInMainHand().getItemMeta()).getPage(1).contains("\u0800") && ((BookMeta) player.getInventory().getItemInMainHand().getItemMeta()).getPageCount() >= 1) {
+            player.sendMessage(ChatColor.RED + "Hey, you...don't do that >:(");
+            player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+        }
+	}
 }
